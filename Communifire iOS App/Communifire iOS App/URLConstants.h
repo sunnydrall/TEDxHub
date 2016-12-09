a82f0fe32788bc5388ee5443059886d445734d62
Raghav Khunger <raghav.khunger@gmail.com>
2016-12-09 16:20:39 +0530

    File missing

---
+++ Communifire iOS App/Communifire iOS App/URLConstants.h
@@ -0,0 +1,19 @@
+//
+//  URLConstants.h
+//  Communifire
+//
+//  Created by Raghav Khunger on 07/12/16.
+//  Copyright © 2016 Axero Solutions LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface URLConstants : NSObject
+
+extern NSString *const DEFAULT_DOMAIN;
+extern NSString *const MYACCOUNT_URL;
+extern NSString *const GET_ME_API_URL;
+extern NSString *const ADD_DEVICE_TOKEN_API_URL;
+
+@end
+
